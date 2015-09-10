385223d70ab9b04176654dcc576e7245704dc905
crisdabruno <crisdabruno@hotmail.com>
2015-09-10 16:40:22 -0300

    ultimo

--- src/ar/edu/unlam/tallerweb1/cuentabancaria/Cuenta.java
+++ src/ar/edu/unlam/tallerweb1/cuentabancaria/Cuenta.java
@@ -9,10 +9,12 @@ public class Cuenta {
 		this.saldo=saldo;
 	}
 	
-	public boolean deposito(double aDepositar){
+	public boolean deposito(double aDepositar)
+	{
 	
 		if (aDepositar>0){
 			this.saldo=this.saldo+ aDepositar;
+			System.out.println("Deposito exitoso, su saldo a la fecha es " + this.saldo);
 			return true;
 		}
 		else{
@@ -22,6 +24,22 @@ public class Cuenta {
 	
 	public double obtenerSaldo(){
 		
-		return this.saldo=saldo;
+		System.out.println("Su saldo a la fecha es " + this.saldo);
+		return this.saldo;
+		
+	}
+	
+	public boolean extrato(double aExtraer)
+	{
+		if (aExtraer <= this.saldo && aExtraer >0) 
+		{
+			this.saldo=this.saldo-aExtraer;
+			System.out.println("Extrato exitoso, su saldo a la fecha es " + this.saldo);
+			return true;
+		}
+		else
+		{
+			return false;	
+		}	
 	}
 }
--- test/ar/edu/unlam/tallerweb1/cuentabancaria/CuentaTest.java
+++ test/ar/edu/unlam/tallerweb1/cuentabancaria/CuentaTest.java
@@ -8,12 +8,28 @@ public class CuentaTest {
 	@Test
 	public void depositoTest() {
 		
-		Cuenta depositar= new Cuenta(0.0);
+		Cuenta depositar= new Cuenta(60.00);
 		depositar.deposito(40.00);
 		
-		Assert.assertTrue(40.00 == depositar.obtenerSaldo());
+		Assert.assertTrue(100.00 == depositar.obtenerSaldo());
 		
 		depositar.deposito(-15);
 		Assert.assertFalse(-15 == depositar.obtenerSaldo());
+	}
+	
+	@Test
+	public void extratoTest(){
+		
+		Cuenta extraer= new Cuenta(300.00);
+		
+		Assert.assertTrue(extraer.extrato(20.00)==true);
+	}
+	
+	
+	@Test
+	public void consultaSaldoTest(){
+		
+		Cuenta consulta=new Cuenta(0.0);		
+		Assert.assertTrue(consulta.obtenerSaldo()==0);
 	}
 }
